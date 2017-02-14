f75436a06d96b520431955815646a4e3c9491145
emppu－cao <759362837@qq.com>
2017-02-14 13:50:20 +0800

    修改数据
    修改数据

--- touchClearDemo/ViewController.m
+++ touchClearDemo/ViewController.m
@@ -42,8 +42,9 @@ static NSString * const cellID = @"cellID";
     
     pictureViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:cellID forIndexPath:indexPath];
     
-//    cell.pictureName = self.allphotos[indexPath.item];
-    cell.picturePath = self.allphotos[indexPath.item];
+    cell.pictureName = self.allphotos[indexPath.item];
+//    cell.picturePath = self.allphotos[indexPath.item];
+    
     return  cell;
 }
 
@@ -80,8 +81,8 @@ static NSString * const cellID = @"cellID";
 - (NSArray *)allphotos
 {
     if (!_allphotos) {
-//        _allphotos = @[@"1", @"2", @"3", @"4", @"5"];
-        _allphotos = @[@"http://img3.duitang.com/uploads/item/201608/06/20160806091136_253BA.jpeg", @"http://pic.58pic.com/58pic/13/12/24/21E58PICfx8_1024.jpg", @"http://imgwww.heiguang.net/uploadfile/2014/0819/20140819115116482.jpg", @"http://img2.ph.126.net/T6OwzNcCb-PTV1kEkvbCnQ==/6597201708051956069.jpg", @"http://img.pconline.com.cn/images/upload/upc/tx/photoblog/1410/21/c4/39944979_39944979_1413878416125_mthumb.jpg",@"http://image92.360doc.com/DownloadImg/2015/12/1715/63143393_2.jpg"];
+        _allphotos = @[@"1", @"2", @"3", @"4", @"5"];
+//        _allphotos = @[@"http://img3.duitang.com/uploads/item/201608/06/20160806091136_253BA.jpeg", @"http://pic.58pic.com/58pic/13/12/24/21E58PICfx8_1024.jpg", @"http://imgwww.heiguang.net/uploadfile/2014/0819/20140819115116482.jpg", @"http://img2.ph.126.net/T6OwzNcCb-PTV1kEkvbCnQ==/6597201708051956069.jpg", @"http://img.pconline.com.cn/images/upload/upc/tx/photoblog/1410/21/c4/39944979_39944979_1413878416125_mthumb.jpg",@"http://image92.360doc.com/DownloadImg/2015/12/1715/63143393_2.jpg"];
     }
     return _allphotos;
 }
